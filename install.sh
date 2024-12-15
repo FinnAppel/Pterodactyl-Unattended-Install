0154308bdceed632aa4d733195db737a03063e7a
Finn <113428508+FinnAppel@users.noreply.github.com>
2024-12-15 19:13:18 +0100

    Update install.sh
    Added wings.service

--- install.sh
+++ install.sh
@@ -197,6 +197,7 @@ GRUB_CMDLINE_LINUX_DEFAULT="swapaccount=1"
 sudo mkdir -p /etc/pterodactyl
 curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
 sudo chmod u+x /usr/local/bin/wings
+curl -s https://raw.githubusercontent.com/FinnAppel/Pterodactyl-Unattended-Install/main/config/wings.service > /etc/systemd/system/wings.service
 
 # Install Pterodactyl Node Database
 echo "Creating Pterodactyl Node Database..."
